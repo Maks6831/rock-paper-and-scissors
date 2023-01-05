4385382152f17c6020954564ff39a52f78a0feba
maudmain <117314424+maudmain@users.noreply.github.com>
2023-01-05 21:36:57 +0000

    for loop+ storage/ display wins/losses added

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 alert('Lets play Rock, paper and Scissors!');
-let userScore = 0
+let userWins = 0;
+let userLosses =0;
+let ties = 0;
+
+for (i= 0; i <10; i++){
+
 let userInput = prompt('What do you choose?');
 let randomComp = Math.random();
 let compInput = '';
@@ -14,34 +19,41 @@ if (randomComp <= 0.33) {
 
 console.log(compInput)
 
-if (compInput === userInput){
-    alert("It's a tie")
+if (compInput === userInput) {
+    alert("It's a tie");
+    ties +=1;
 }
 
-if (userInput === "rock"){
-    if (compInput === "paper"){
-        alert("You lose") 
-    } else if (compInput === "scissors"){
-        alert("You win!")
-        userScore += 1
+if (userInput === "rock") {
+    if (compInput === "paper") {
+        alert("You lose")
+        userLosses +=1;
+    } else if (compInput === "scissors") {
+        alert("You win!");
+        userWins += 1;
     }
 }
 
-if (userInput === "paper"){
-    if (compInput === "scissors"){
-        alert("You lose") 
-    } else if (compInput === "rock"){
-        alert("You win!")
-        userScore += 1
+if (userInput === "paper") {
+    if (compInput === "scissors") {
+        alert("You lose")
+        userLosses +=1;
+    } else if (compInput === "rock") {
+        alert("You win!");
+        userWins += 1;
     }
 }
 
-if (userInput === "scissors"){
-    if (compInput === "rock"){
-        alert("You lose") 
-    } else if (compInput === "paper"){
-        alert("You win!")
-        userScore += 1
+if (userInput === "scissors") {
+    if (compInput === "rock") {
+        alert("You lose")
+        userLosses +=1;
+    } else if (compInput === "paper") {
+        alert("You win!");
+        userWins += 1;
     }
 }
-console.log(userScore)
+}
+console.log("Your number of wins is " + userWins);
+console.log("Your number of losses is " + userLosses);
+console.log("The number of ties is " + ties);
