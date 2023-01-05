6405f55c0635e3e843b7223d41330ad503962a9a
maudmain <117314424+maudmain@users.noreply.github.com>
2023-01-05 20:32:20 +0000

    user if conditions added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 alert('Lets play Rock, paper and Scissors!');
+let userScore = 0
 let userInput = prompt('What do you choose?');
 let randomComp = Math.random();
 let compInput = '';
@@ -13,3 +14,34 @@ if (randomComp <= 0.33) {
 
 console.log(compInput)
 
+if (compInput === userInput){
+    alert("It's a tie")
+}
+
+if (userInput === "rock"){
+    if (compInput === "paper"){
+        alert("You lose") 
+    } else if (compInput === "scissors"){
+        alert("You win!")
+        userScore += 1
+    }
+}
+
+if (userInput === "paper"){
+    if (compInput === "scissors"){
+        alert("You lose") 
+    } else if (compInput === "rock"){
+        alert("You win!")
+        userScore += 1
+    }
+}
+
+if (userInput === "scissors"){
+    if (compInput === "rock"){
+        alert("You lose") 
+    } else if (compInput === "paper"){
+        alert("You win!")
+        userScore += 1
+    }
+}
+console.log(userScore)
